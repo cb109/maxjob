c35509e10955c7abe8bbb6ff2db41c220d9ec058
Christoph Buelter <c.buelter@arcor.de>
2015-10-31 02:44:07 +0100

    Add logfile watchers. Remove unused protocol methods.

--- maxjob/__init__.py
+++ maxjob/__init__.py
@@ -1,5 +1,11 @@
+import os
+import signal
+import sys
+
 from twisted.internet import protocol
 from twisted.internet import reactor
+
+from maxjob.watcher import create_logfile_watcher
 
 
 class MaxJobProcessProtocol(protocol.ProcessProtocol):
@@ -7,9 +13,6 @@ class MaxJobProcessProtocol(protocol.ProcessProtocol):
     def __init__(self):
         self.stdout_history = []
         self.stderr_history = []
-
-    def connectionMade(self):
-        print "connectionMade!"
 
     def outReceived(self, data):
         lines = data.split("\n")
@@ -27,13 +30,6 @@ class MaxJobProcessProtocol(protocol.ProcessProtocol):
         for line in lines:
             print "stderr:", line
 
-    def outConnectionLost(self):
-        print "outConnectionLost! The child closed their stdout!"
-        # (dummy, lines, words, chars, file) = re.split(r'\s+', self.data)
-
-    def errConnectionLost(self):
-        print "errConnectionLost! The child closed their stderr."
-
     def processExited(self, reason):
         print "processExited, status %d" % (reason.value.exitCode,)
 
@@ -42,22 +38,49 @@ class MaxJobProcessProtocol(protocol.ProcessProtocol):
         print "quitting"
         reactor.stop()
 
+    # def suicide(self):
+    #     self.transport.signalProcess(signal.SIGTERM)
+    #     self.transport.signalProcess(signal.SIGKILL)
+    #     self.transport.signalProcess("KILL")
 
-def main():
+
+def test():
     maxbinary = r"C:\Program Files\Autodesk\3ds Max 2015\3dsmax.exe"
-    scriptfile = r"C:\Users\Buelter\Google Drive\dev\maxjob\maxjob\backend.ms"
-    stdoutfile = r"C:\Users\Buelter\Desktop\stdout.txt"
-    # maxlogfile = r"C:\Users\Buelter\Desktop\maxjob.log"
+    maxjob_backend = r"C:\Users\Buelter\Google Drive\dev\maxjob\maxjob\backend.ms"
 
-    pp = MaxJobProcessProtocol()
-    reactor.spawnProcess(pp,
-                         maxbinary,
-                         args=["3dsmax.exe", "-U", "MAXScript", scriptfile],
-                         env={})
+    network_logfile = r"C:\Users\Buelter\AppData\Local\Autodesk\3dsMax\2015 - 64bit\ENU\Network\Max.log"
+    mxs_logfile = os.path.abspath("maxjob.log")
+
+    def print_diff(diff):
+        if diff:
+            print diff
+
+    # Observe logfiles for changes.
+    watchers = [create_logfile_watcher(network_logfile, print_diff),
+                create_logfile_watcher(mxs_logfile, print_diff)]
+    for watcher in watchers:
+        watcher.start()
+
+    # Launch 3ds Max and listen to its standard channels.
+    protocol = MaxJobProcessProtocol()
+    args = ["3dsmax.exe", "-U", "MAXScript", maxjob_backend]
+    env = os.environ.update({"MAXJOB_BACKEND_LOGFILE": mxs_logfile})
+
+    # def register_interrupt_handler():
+    #     def cleanup(signal_, frame):
+    #         print "Performing cleanup work, hang on..."
+    #         protocol.suicide()
+    #         reactor.stop()
+    #         for watcher in watchers:
+    #             watcher.stop()
+    #         sys.exit(0)
+    #     signal.signal(signal.SIGINT, cleanup)
+
+    # register_interrupt_handler()
+
+    reactor.spawnProcess(protocol, maxbinary, args=args, env=env)
     reactor.run()
 
 
-
-
 if __name__ == '__main__':
-    main()
+    test()
