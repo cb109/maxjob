e80d35be04f42fb323103cda919562e25baec602
Christoph Buelter <c.buelter@arcor.de>
2015-10-31 22:49:31 +0100

    Bump version to 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name="maxjob",
-                 version="0.1.0",
+                 version="0.1.1",
                  packages=setuptools.find_packages(),
                  install_requires=["pypiwin32",
                                    "twisted",
