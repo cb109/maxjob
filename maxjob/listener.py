35acab7f54de467c2c68814e6cbba0923fb65f8f
Christoph Buelter <c.buelter@arcor.de>
2015-10-31 01:17:06 +0100

    Implement a simple LogFileChangedHandler.

--- maxjob/listener.py
+++ maxjob/listener.py
@@ -1,35 +1,57 @@
 import os
 import time
-import logging
 
-from watchdog import observers
-from watchdog import events
+from watchdog import events, observers
 
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+class LogFileChangedHandler(events.PatternMatchingEventHandler):
+    """Watch a logfile for changes (file must exist).
+
+    We assume that this logfile is only modified in that lines are
+    added to it, not modified or removed. This really simplifies the
+    diff. Whenever that happens, this handler will notice and notify the
+    subscriber(s) via the callback.
+
+    """
+    def __init__(self, filepath, callback, *args, **kwargs):
+        kwargs["patterns"] = [filepath]  # Watch only that single file.
+        super(LogFileChangedHandler, self).__init__(*args, **kwargs)
+        self.filepath = filepath
+        self.callback = callback
+        try:
+            self.last_content = open(self.filepath).read().strip()
+        except IOError:
+            self.last_content = ""
+
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        with open(event.src_path) as f:
+            content = f.read()
+        diff = content.replace(self.last_content, "", 1)
+        self.last_content = content
+        self.callback(diff)
 
 
-class MaxLogfileEventHandler(events.PatternMatchingEventHandler):
+def test():
+    maxlogfile = "maxjob.log"
+    logfiledir = os.path.dirname(maxlogfile)
 
-    def on_any_event(self, event):
-        print "something happened:", event
+    def print_diff(diff):
+        if diff:
+            print diff
+
+    eventhandler = LogFileChangedHandler(maxlogfile, print_diff)
+    observer = observers.Observer()
+    observer.schedule(eventhandler, logfiledir)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
-maxlogfile = r"C:\Users\Bueltewwwwr\Desktop\maxjob.log"
-logfiledir = os.path.dirname(maxlogfile)
-
-# eventhandler = events.LoggingEventHandler()
-# eventhandler = events.PatternMatchingEventHandler(patterns=[maxlogfile])
-eventhandler = MaxLogfileEventHandler(patterns=[maxlogfile])
-
-observer = observers.Observer()
-observer.schedule(eventhandler, logfiledir)
-observer.start()
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
+if __name__ == '__main__':
+    test()
