32211dee23422df1ccd96a89c50a275150159308
Christoph Buelter <c.buelter@arcor.de>
2015-11-01 03:25:31 +0100

    Move requirements to textfile and extend setup.py with metadata. Add pyinstaller dependency.

--- setup.py
+++ setup.py
@@ -7,8 +7,13 @@ import setuptools
 setuptools.setup(
     name="maxjob",
     version="0.1.11",
+    description="Start 3ds Max from the commandline to do a maxscript job.",
+    long_description=open("README.rst").read(),
+    author="Christoph Buelter",
+    author_email="info@cbuelter.de",
+    url="https://github.com/cb109/maxjob.git",
+    keywords="3d, 3dsmax, 3dsmaxcmd, maxscript, dcc, render, batch",
     packages=setuptools.find_packages(),
-    install_requires=["pypiwin32", "twisted", "psutil",
-                      "easydict", "click", "watchdog"],
+    install_requires=open("requirements.txt").readlines(),
     entry_points={"console_scripts": ["maxjob=maxjob.__main__:cli"]}
 )
